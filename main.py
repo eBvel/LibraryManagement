08cd2206fe3519b0be8c3af5acf59ab6de6a9c0a
eBvel <belyakov041098@gmail.com>
2025-07-12 18:52:10 +0700

    Create the 'find_book' method.

--- main.py
+++ main.py
@@ -125,6 +125,17 @@ def return_book(library, title):
         print(f"Книги '{title}' уже в наличии!")
 
 
+def find_book(library, title):
+    if is_book_in_library(library, title):
+        print(get_book_data(title, library[title]))
+
+
+def get_book_data(title, book):
+    return (f"Название книги: {title}\nАвтор: {book[author_key]}\n"
+            f"Год выпуска: {book[year_key]}\nВ наличие: "
+            f"{'есть' if book[available_key] else 'нет'}")
+
+
 def to_ask(question):
     answers = {'да': True, 'нет': False}
     while True:
@@ -157,6 +168,11 @@ def return_book_to_library(library):
     return_book(library, title)
 
 
+def find_book_in_library(library):
+    title = request_book_title()
+    find_book(library, title)
+
+
 def complete_program():
     global at_work
     at_work = False
@@ -172,29 +188,43 @@ def start_menu():
  3. Удалить книгу;
  4. Выдать книгу читателю;
  5. Возврат книги в бибилиотеку;
- 6. Выход из программы.""")
+ 6. Найти книгу по названию;
+ 7. Выход из программы.""")
 
 
 def main():
     library = {
-        "Преступление и наказание": {author_key: "Федор Достоевский",
-                                     year_key: 1990,
-                                     available_key: False},
-        "Идиот": {author_key: "Федор Достоевский", year_key: 1998,
-                  available_key: True},
-        "Капитанская дочка": {author_key: "Александр Пушкин",
-                              year_key: 2001, available_key: False},
-        "Вишневый сад": {author_key: "Антон Чехов", year_key: 1995,
-                         available_key: True},
-        "Отцы и дети": {author_key: "Иван Тургенев", year_key: 2003,
-                        available_key: True}
+        "Преступление и наказание": {
+            author_key: "Федор Достоевский",
+            year_key: 1990,
+            available_key: False
+        },
+        "Идиот": {
+            author_key: "Федор Достоевский",
+            year_key: 1998,
+            available_key: True
+        },
+        "Капитанская дочка": {
+            author_key: "Александр Пушкин",
+            year_key: 2001,
+            available_key: False
+        },
+        "Вишневый сад": {
+            author_key: "Антон Чехов",
+            year_key: 1995,
+            available_key: True
+        },
+        "Отцы и дети": {
+            author_key: "Иван Тургенев",
+            year_key: 2003,
+            available_key: True
+        }
     }
-
     while at_work:
         menu_operations = {1: book_list_view, 2: add_book_to_library,
                            3: remove_book_from_library,
                            4: issue_book_to_reader, 5: return_book_to_library,
-                           6: complete_program}
+                           6: find_book_in_library, 7: complete_program}
         request_menu_item(menu_operations, library)
 
 
