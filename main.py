88bf684ca2085351c663fd4e4b90c29226003d64
eBvel <belyakov041098@gmail.com>
2025-07-12 14:14:46 +0700

    Create the 'add_book' method to add a new book to the library.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def book_list_view(library):
+def book_list_view():
     if library:
         print("\n-----СПИСОК КНИГ-----")
         for book in library.keys():
@@ -7,22 +7,52 @@ def book_list_view(library):
         print("\nВ библиотеке отсутствуют книги!")
 
 
+def add_book(title, author, year):
+    def perform_addition(text):
+        new_book = {title: {author_key: author, year_key: year,
+                            available_key: None}}
+        library.update(new_book)
+        print(text)
+
+    if title in library:
+        answer = to_ask(f"Книга '{title}' уже есть в библиотеке.\nЖелаете "
+                        f"обновить данные?")
+        if answer:
+            perform_addition(f"Данные книги '{title}' - обновлены!")
+        else:
+            print("Обновление отменено!")
+    else:
+        perform_addition(f"Книга '{title}' - добавлена в библиотеку!")
+
+
+def to_ask(question):
+    answers = {'да': True, 'нет': False}
+    while True:
+        try:
+            answer = input(f"{question} (да/нет): ").lower()
+            return answers[answer]
+        except KeyError:
+            print("Ошибка: введен некорректный ответ!")
+
+
 def main():
-    library = {
-        "Преступление и наказание": {'author': "Федор Достоевский",
-                                     'publish_date': 1990,
-                                     'is_available': False},
-        "Идиот": {'author': "Федор Достоевский", 'publish_date': 1998,
-                  'is_available': True},
-        "Капитанская дочка": {'author': "Александр Пушкин",
-                              'publish_date': 2001, 'is_available': False},
-        "Вишневый сад": {'author': "Антон Чехов", 'publish_date': 1995,
-                         'is_available': True},
-        "Отцы и дети": {'author': "Иван Тургенев", 'publish_date': 2003,
-                        'is_available': True}
-    }
-
-    book_list_view(library)
+    book_list_view()
 
 
+author_key = 'author'
+year_key = 'publish_date'
+available_key = 'is_available'
+library = {
+    "Преступление и наказание": {author_key: "Федор Достоевский",
+                                 year_key: 1990,
+                                 available_key: False},
+    "Идиот": {author_key: "Федор Достоевский", year_key: 1998,
+              available_key: True},
+    "Капитанская дочка": {author_key: "Александр Пушкин",
+                          year_key: 2001, available_key: False},
+    "Вишневый сад": {author_key: "Антон Чехов", year_key: 1995,
+                     available_key: True},
+    "Отцы и дети": {author_key: "Иван Тургенев", year_key: 2003,
+                    available_key: True}
+}
 main()
